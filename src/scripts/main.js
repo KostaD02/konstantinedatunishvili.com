e11313e5ebef718f0439ce8fd22e4a9f5c01e266
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-07-24 18:43:21 +0400

    feat: header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log("test");
+(function () {
+  function init() {
+    if (isLocalDevelopment()) {
+      appendLocalStyles();
+      console.log();
+    }
+  }
+
+  function isLocalDevelopment() {
+    return window.location.hostname === "localhost";
+  }
+
+  function appendLocalStyles() {
+    const styles = [
+      "../../src/styles/typography.css",
+      "../../src/styles/style.css",
+    ];
+    styles.forEach((href) => {
+      const style = document.createElement("style");
+      style.setAttribute("rel", "stylesheet");
+      style.setAttribute("href", href);
+      document.head.appendChild(style);
+    });
+  }
+
+  init();
+})();
