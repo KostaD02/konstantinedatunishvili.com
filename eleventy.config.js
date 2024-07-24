882f576991ca6858d2b8ffd5ff37be5d7e29f007
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-07-24 18:43:38 +0400

    feat: header

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,8 @@
+const eleventySass = require("eleventy-sass");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventySass);
+
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/scripts");
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,73 @@
-(function () {
-  function init() {
-    if (isLocalDevelopment()) {
-      appendLocalStyles();
-      console.log();
+function init() {
+  const header = document.querySelector("header");
+  const menu = document.querySelector("button.menu");
+
+  let lastScrollPos = 0;
+
+  function handleScroll() {
+    const scrollPosition = document.documentElement.scrollTop;
+
+    if (scrollPosition > lastScrollPos) {
+      header.classList.add("hide");
+      header.classList.add("scrolled");
+    } else if (scrollPosition === 0) {
+      header.classList.remove("hide");
+      header.classList.remove("scrolled");
+    } else {
+      header.classList.add("scrolled");
+      header.classList.remove("hide");
+    }
+
+    lastScrollPos = scrollPosition;
+  }
+
+  function handleResize() {
+    const isOpen = document.querySelector(".open");
+    if (isOpen && window.innerWidth > 768) {
+      isOpen.click();
     }
   }
 
-  function isLocalDevelopment() {
-    return window.location.hostname === "localhost";
+  function handleMenuClick() {
+    this.classList.toggle("open");
+    const isOpen = this.classList.contains("open");
+    const parent = this;
+
+    if (isOpen) {
+      header.classList.add("hide-colors");
+      const modal = document.createElement("div");
+      modal.classList.add("navigation-modal");
+      const ul = document.createElement("ul");
+      ul.innerHTML = header.querySelector("ul").innerHTML;
+      modal.appendChild(ul);
+      modal.addEventListener("click", (event) => {
+        if (
+          event.target.classList.contains("navigation-modal") ||
+          event.target.tagName === "A"
+        ) {
+          parent.click();
+        }
+      });
+      header.appendChild(modal);
+      document.body.style.overflow = "hidden";
+      return;
+    }
+
+    const navigationModal = document.querySelector(".navigation-modal");
+    navigationModal.classList.add("fade-out");
+    navigationModal.querySelector("ul").classList.add("slide-out");
+
+    setTimeout(() => {
+      navigationModal.remove();
+    }, 500);
+
+    document.body.style.overflow = "auto";
+    header.classList.remove("hide-colors");
   }
 
-  function appendLocalStyles() {
-    const styles = [
-      "../../src/styles/typography.css",
-      "../../src/styles/style.css",
-    ];
-    styles.forEach((href) => {
-      const style = document.createElement("style");
-      style.setAttribute("rel", "stylesheet");
-      style.setAttribute("href", href);
-      document.head.appendChild(style);
-    });
-  }
+  menu.addEventListener("click", handleMenuClick);
+  window.addEventListener("scroll", handleScroll);
+  window.addEventListener("resize", handleResize);
+}
 
-  init();
-})();
+window.addEventListener("DOMContentLoaded", init);
