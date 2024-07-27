c907d98b88b75afc18f9b692582a5b3a1301881f
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-07-27 22:17:28 +0400

    feat: archive

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,9 +5,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/scripts");
-
   eleventyConfig.addPassthroughCopy("src/**/*.css");
-  eleventyConfig.addPassthroughCopy("src/**/*.css.map");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
